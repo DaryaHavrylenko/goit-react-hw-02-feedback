75a90da6a8f5cf24a7b25fbe4905f311e155bce7
Darya <HavrylenkoIT@gmail.com>
2022-10-30 19:29:46 +0200

    step 2 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,13 +9,33 @@ state = {
   bad: 0
 }
   countFeedback = (event) => {
+ 
     this.setState(prevState => {
       return {
+        ...this.state,
       [event.target.name]: prevState[event.target.name] + 1,
        }
    })
   }
   
+  countTotalFeedback = () => {
+    const values = Object.values(this.state);
+  
+    let total = 0;
+    for (const value of values) {
+      total += value;
+     
+    }
+    return total;
+  }
+
+  countPositiveFeedbackPercentage = () => {
+   
+    const total = this.state.good + this.state.neutral + this.state.bad;
+   const Percentege = Number(((this.state.good * 100) / (total)).toFixed(0));
+  
+    return Percentege;
+}
 
  render() {
    return (<div>
@@ -29,7 +49,9 @@ state = {
      <div>
        <p>Good:<span>{this.state.good}</span></p>
        <p>Neutral:<span>{this.state.neutral}</span></p>
-      <p>Bad:<span>{ this.state.bad}</span></p>
+       <p>Bad:<span>{this.state.bad}</span></p>
+       <p>Total:<span>{this.countTotalFeedback()}</span></p>
+       <p>Positive Feedback:<span>{this.countPositiveFeedbackPercentage() || 0? this.countPositiveFeedbackPercentage() : 0}%</span></p>
      </div>
    </div>
   
